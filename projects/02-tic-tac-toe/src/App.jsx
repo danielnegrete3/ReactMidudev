8b7dc1d9fd8a57372463b03615c3f5d4c18f5db0
Daniel Negrete <daniel.negrete5666@alumnos.udg.mx>
2024-09-04 11:26:46 -0600

    add eslint to the project

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -8,34 +8,34 @@ import { WinnerModal } from './components/WinnerModal.jsx';
 function App() {
 
   const [board, setBoard] = useState(
-    ()=>{
+    () => {
       const boardFromStorage = window.localStorage.getItem('board');
       return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null);
     }
   );
   const [turn, setTurn] = useState(
-    ()=>{
+    () => {
       const turnFormStorage = window.localStorage.getItem('turn');
-      return turnFormStorage? JSON.parse(turnFormStorage): TURNS.X;
+      return turnFormStorage ? JSON.parse(turnFormStorage) : TURNS.X;
     }
   );
   const [winner, setWinner] = useState(null);
 
 
 
-  const updateBoard = (index) =>{
+  const updateBoard = (index) => {
 
     // si ya hay algo 
-    if(board[index] || winner) return;
+    if (board[index] || winner) return;
 
     // actualizar el tablero
     var newBord = [...board];
     newBord[index] = turn;
 
-    var win = checkWinner(newBord,index)
-    if(win != null){
+    var win = checkWinner(newBord, index)
+    if (win != null) {
       // newBord = Array(9).fill(null);
-      if(win){
+      if (win) {
         confetti()
       }
       setWinner(win);
@@ -45,7 +45,7 @@ function App() {
     setBoard(newBord);
 
     // cambiar turno
-    const newTurn = turn == TURNS.X? TURNS.O:TURNS.X;
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
     // guardar partida 
@@ -53,12 +53,12 @@ function App() {
     window.localStorage.setItem('turn', JSON.stringify(newTurn));
   }
 
-  const resetGame = ()=>{
-      setBoard(Array(9).fill(null));
-      setTurn(TURNS.X);
-      setWinner(null);
-      window.localStorage.removeItem('board');
-      window.localStorage.removeItem('turn');
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+    window.localStorage.removeItem('board');
+    window.localStorage.removeItem('turn');
   }
 
   return (
@@ -66,13 +66,13 @@ function App() {
       <h1 className=' mb-4'>Tic tac toe</h1>
       <button onClick={resetGame} className='  px-2 py-3 m-6 bg-transparent border-solid border-[#eee] border-2 rounded-md font-bold cursor-pointer hover:bg-[#eee] hover:text-[#222] '>
         Reset del juego
-      </button> 
+      </button>
       <section className=' grid grid-cols-3 gap-3'>
         {
           board.map(
-            (_,index) =>{
-              return(
-                <Square 
+            (_, index) => {
+              return (
+                <Square
                   key={index}
                   index={index}
                   updateBoard={updateBoard}
