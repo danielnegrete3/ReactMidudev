db1c0da7ec02f0c566b735aa56bfc9aa7cf5ace3
Daniel Negrete <daniel.negrete5666@alumnos.udg.mx>
2024-09-04 13:41:55 -0600

    Finish the 2th video

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react'
+import { FollowMous } from './components/FollowMouse'
+
+function App() {
+  return (
+    <main>
+      <FollowMous />
+    </main>
+  )
+}
+
+export default App
